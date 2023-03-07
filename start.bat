f719489250a29ea696e5b783f20d3d7add99edf9
tim0-12432 <79634593+tim0-12432@users.noreply.github.com>
2023-03-07 15:56:41 +0100

    echo off

--- start.bat
+++ start.bat
@@ -1,6 +1,8 @@
 :: --------------------------------------------
 :: This file is for easier automation purposes.
 :: --------------------------------------------
+@echo off
 echo %date% %time% >> %~dp0\log.txt
 %~dp0\.venv\Scripts\python.exe %~dp0\generator.py %* >> %~dp0\log.txt 2>&1
 echo ---------------------- >> %~dp0\log.txt
+exit
